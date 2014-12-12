373c0aedc367debe4324366202915c810db63ac6
lnznt <lnznt0+github@gmail.com>
2014-12-13 00:07:29 +0900

    first commit

---
+++ lib/camelizable.rb
@@ -0,0 +1,11 @@
+require "camelizable/version"
+
+module Camelizable
+  def ucc(sep=/[^[:alnum:]]+/)
+    split(sep).map(&:capitalize).join
+  end
+
+  def lcc(sep=/[^[:alnum:]]+/)
+    ucc(sep).sub(/./){ $&.downcase }
+  end
+end
---
+++ lib/camelizable/version.rb
@@ -0,0 +1,3 @@
+module Camelizable
+  VERSION = "0.0.1"
+end
